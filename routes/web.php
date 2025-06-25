048b8423bc84eae7049b04f4f73d858bd3fec71f
Ignacio Mersch <ignaciomersch602811@gmail.com>
2025-06-25 20:47:44 -0300

    #Add: modelos(cliente, pago, factura)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Models\Pago;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -18,3 +19,14 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+// Route::get('prueba', function (){
+//     $pago = new Pago;
+//     $pago->monto = 12.12;
+//     $pago->fecha_pago = "12/12/12";
+//     $pago->metodo_pago = "Efectivo";
+//     $pago->save();
+
+
+//     return $pago;
+// });
